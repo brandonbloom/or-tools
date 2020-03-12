a76ae54edbf0a62bad14e60eb4bacaa8c6dc019a
Brandon Bloom <brandon@brandonbloom.name>
2020-03-12 14:30:04 -0700

    linear-programming.js starting to work

--- examples/javascript/linear-programming.js
+++ examples/javascript/linear-programming.js
@@ -17,67 +17,75 @@
 // XXX const lp = require('ortools/linear_solver');
 const lp = require('linear_solver');
 
-console.log({lp}); // XXX
-
 const main = () => {
   // Instantiate a Glop solver, naming it LinearExample.
   const solver = new lp.Solver('LinearExample', lp.Solver.GLOP_LINEAR_PROGRAMMING);
 
   // Create the two variables and let them take on any value.
-  // XXX const x = new solver.NumVar(-solver.infinity(), solver.infinity(), 'x')
-  // XXX const y = new solver.NumVar(-solver.infinity(), solver.infinity(), 'y')
+  const x = solver.NumVar(-Infinity, Infinity, 'x');
+  const y = solver.NumVar(-Infinity, Infinity, 'y');
 
   // Objective function: Maximize 3x + 4y.
-// XXX  const objective = new solver.Objective()
-// XXX    objective.SetCoefficient(x, 3)
-// XXX    objective.SetCoefficient(y, 4)
-// XXX    objective.SetMaximization()
-// XXX
-// XXX    # Constraint 0: x + 2y <= 14.
-// XXX    constraint0 = solver.Constraint(-solver.infinity(), 14)
-// XXX    constraint0.SetCoefficient(x, 1)
-// XXX    constraint0.SetCoefficient(y, 2)
-// XXX
-// XXX    # Constraint 1: 3x - y >= 0.
-// XXX    constraint1 = solver.Constraint(0, solver.infinity())
-// XXX    constraint1.SetCoefficient(x, 3)
-// XXX    constraint1.SetCoefficient(y, -1)
-// XXX
-// XXX    # Constraint 2: x - y <= 2.
-// XXX    constraint2 = solver.Constraint(-solver.infinity(), 2)
-// XXX    constraint2.SetCoefficient(x, 1)
-// XXX    constraint2.SetCoefficient(y, -1)
-// XXX
-// XXX    print('Number of variables =', solver.NumVariables())
-// XXX    print('Number of constraints =', solver.NumConstraints())
-// XXX
-// XXX    # Solve the system.
-// XXX    status = solver.Solve()
-// XXX    # Check that the problem has an optimal solution.
-// XXX    if status != pywraplp.Solver.OPTIMAL:
-// XXX        print("The problem does not have an optimal solution!")
-// XXX        exit(1)
-// XXX
-// XXX    print('Solution:')
-// XXX    print('x =', x.solution_value())
-// XXX    print('y =', y.solution_value())
-// XXX    print('Optimal objective value =', objective.Value())
-// XXX    print('')
-// XXX    print('Advanced usage:')
-// XXX    print('Problem solved in ', solver.wall_time(), ' milliseconds')
-// XXX    print('Problem solved in ', solver.iterations(), ' iterations')
-// XXX    print('x: reduced cost =', x.reduced_cost())
-// XXX    print('y: reduced cost =', y.reduced_cost())
-// XXX    activities = solver.ComputeConstraintActivities()
-// XXX    print('constraint0: dual value =',
-// XXX          constraint0.dual_value(), ' activities =',
-// XXX          activities[constraint0.index()])
-// XXX    print('constraint1: dual value =',
-// XXX          constraint1.dual_value(), ' activities =',
-// XXX          activities[constraint1.index()])
-// XXX    print('constraint2: dual value =',
-// XXX          constraint2.dual_value(), ' activities =',
-// XXX          activities[constraint2.index()])
+  const objective = solver.Objective();
+  objective.SetCoefficient(x, 3);
+  objective.SetCoefficient(y, 4);
+  objective.SetMaximization();
+
+  // Constraint 0: x + 2y <= 14.
+  constraint0 = solver.Constraint(-Infinity, 14);
+  constraint0.SetCoefficient(x, 1);
+  constraint0.SetCoefficient(y, 2);
+
+  // Constraint 1: 3x - y >= 0.
+  constraint1 = solver.Constraint(0, Infinity);
+  constraint1.SetCoefficient(x, 3);
+  constraint1.SetCoefficient(y, -1);
+
+  // Constraint 2: x - y <= 2.
+  constraint2 = solver.Constraint(-Infinity, 2);
+  constraint2.SetCoefficient(x, 1);
+  constraint2.SetCoefficient(y, -1);
+
+  console.log('Number of variables =', solver.NumVariables());
+  console.log('Number of constraints =', solver.NumConstraints());
+
+  // Solve the system.
+  const status = solver.Solve()
+  // Check that the problem has an optimal solution.
+  if (status !== lp.Solver.OPTIMAL) {
+    console.log("The problem does not have an optimal solution!");
+    process.exit(1);
+  }
+
+  console.log('Solution:');
+  console.log('x =', x.solution_value());
+  console.log('y =', y.solution_value());
+  console.log('Optimal objective value =', objective.Value());
+  console.log('');
+  console.log('Advanced usage:');
+  console.log('Problem solved in ', solver.wall_time(), ' milliseconds');
+  console.log('Problem solved in ', solver.iterations(), ' iterations');
+  console.log('x: reduced cost =', x.reduced_cost());
+  console.log('y: reduced cost =', y.reduced_cost());
+  const activities = solver.ComputeConstraintActivities(); // XXX indexing.
+  console.log(
+    'constraint0: dual value =',
+    constraint0.dual_value(),
+    ' activities =',
+    activities[constraint0.index()],
+  );
+  console.log(
+    'constraint1: dual value =',
+    constraint1.dual_value(),
+    ' activities =',
+    activities[constraint1.index()]
+  );
+  console.log(
+    'constraint2: dual value =',
+    constraint2.dual_value(),
+    ' activities =',
+    activities[constraint2.index()]
+  );
 };
 
 main();
